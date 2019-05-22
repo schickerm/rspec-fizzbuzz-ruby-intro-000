dddf8415a55c7b8ecf8a2dfa41bd0965a447d08d
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-22 16:34:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(number)
-  if 
+def fizzbuzz(num)
+  if num / 3 ==
   
   
   
