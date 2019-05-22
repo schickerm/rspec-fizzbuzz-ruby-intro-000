d62bb73f337b3b2927211eb2e606ae77c81fe098
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-22 15:28:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz
   fizz_3 = fizzbuzz(3)
-  expect(fizz_3).to eq("Fizz")
+     expect(fizz_3).to eq("Fizz")
   
   fizz_5 = fizzbuzz(5)
      expect(fizz_5).to eq("Buzz")
